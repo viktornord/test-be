ebca27b2a2a94ea4c1b211c024d06e90f233e641
viktornord1 <viktornord1@gmail.com>
2018-06-24 15:16:11 +0300

    fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(routes);
+app.use('api', routes);
 
 app.use((req, res, next) => {
   res.status(404).send({
